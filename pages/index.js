0cb43ff0ecc6f9c1eeff563541fe1867d7141ba6
Claire Lebastard <clebastard@fr.ibm.com>
2021-02-16 15:16:28 +0100

    feat: content added
    all content under first tab "A propos"

--- pages/index.js
+++ pages/index.js
@@ -63,59 +63,79 @@ const Home = () => (
           copy: "Lorem ipsum dolor sit ametttt",
         }}
         items={[
-          // Fiches métiers ( 2 fiches)
           {
             type: "ContentGroupCards",
-            heading: "Content Group - with Cards",
+            heading: "Nos métiers",
             items: [
               {
                 heading:
-                  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt",
+                  "Technicien itinérant en systèmes d’alarmes",
                 copy:
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "Nos technicien(ne)s itinérants installent des systèmes d’alarmes chez des particuliers et des petites entreprises. Principales missions : rédiger le diagnostic sécuritaire en y consignant les souhaits et réserves du client, réaliser les tests attestant le bon fonctionnement du système d’alarme installé, expliquer le fonctionnement du système à l’abonné, intervenir en cas de défauts techniques du système chez l’abonné, assurer le remplacement des pièces.",
                 cta: {
                   href: "https://www.example.com",
                 },
               },
               {
                 heading:
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                  "Technicien itinérant en informatique et monétique",
+                copy: "Nos technicien(ne)s interviennent dans des succursales bancaires ou commerçants en garantissant la qualité de service et la satisfaction de nos clients. Principales missions: assurer des missions d’installation et/ou de dépannage selon les instructions et les procédures définies, conseiller et assister les utilisateurs, procéder aux diagnostics des pannes, appliquer une procédure de tests aux différents stades de l’intervention, réaliser les actions administratives, informatiques et logistiques.",
                 cta: {
                   href: "https://www.example.com",
                 },
-              },
-              {
-                heading:
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                copy: "Lorem ipsum dolor sit amet",
-                cta: {
-                  href: "https://www.example.com",
-                },
-              },
-              {
-                heading:
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                copy:
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-                cta: {
-                  href: "https://www.example.com",
-                },
-              },
+              }
             ],
           },
           {
-            type: "ContentGroupPictograms",
-            heading: "Content Group - with Pictograms",
+            type: "ContentGroupSimple",
+            mediaType: "image",
+            mediaData: {
+              heading: "Lorem ipsum dolor sit amet.",
+              image: {
+                sources: [
+                  {
+                    src:
+                      "https://media.istockphoto.com/photos/young-woman-entering-authorization-code-picture-id822123538?s=612x612",
+                    breakpoint: 320,
+                  },
+                  {
+                    src:
+                      "https://media.istockphoto.com/photos/young-woman-entering-authorization-code-picture-id822123538?s=612x612",
+                    breakpoint: 400,
+                  },
+                  {
+                    src:
+                      "https://media.istockphoto.com/photos/young-woman-entering-authorization-code-picture-id822123538?s=612x612",
+                    breakpoint: 672,
+                  },
+                ],
+                alt: "Image alt text",
+                defaultSrc:
+                  "https://media.istockphoto.com/photos/young-woman-entering-authorization-code-picture-id822123538?s=612x612",
+              },
+            },
+            heading: "Pourquoi nous rejoindre",
             items: [
               {
-                heading: "Aliquam condimentum interdum",
+                //heading: "Pour toutes ces raisons :",
                 copy:
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et ultricies est. Mauris iaculis eget dolor nec hendrerit. Phasellus at elit sollicitudin, sodales nulla quis, consequat libero.",
+                  "Nous sommes une entreprise dynamique et en perpétuelle évolution. OIS est à l’écoute de ses salariés. Nous développons les compétences de nos collaborateurs. Nos techniciens sont autonomes. Nous disposons d’un CE. Nous avons mis en place le 1% logement. Participation aux bénéfices",
+              },
+            ],
+          },
+
+          {
+            type: "ContentGroupPictograms",
+            heading: "Notre processus de recrutement",
+            items: [
+              {
+                heading: "Réception et analyse des CV",
+                copy:
+                  "Nos offres d’emplois et notre entreprise vous intéressent et vous postulez à l’adresse suivante : charlotte.ruiz@o-i-s.fr",
                 cta: {
-                  href: "https://www.example.com",
+                  href: "mailto:charlotte.ruiz@o-i-s.fr",
                   type: "local",
-                  copy: "Lorem ipsum dolor",
+                  copy: "Envoyer votre candidature",
                 },
                 pictogram: {
                   src: Desktop,
@@ -123,9 +143,9 @@ const Home = () => (
                 },
               },
               {
-                heading: "Aliquam condimentum interdum",
+                heading: "Entretien téléphonique",
                 copy:
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et ultricies est. Mauris iaculis eget dolor nec hendrerit. Phasellus at elit sollicitudin, sodales nulla quis, consequat libero.",
+                  "Votre candidature a retenu notre attention et nous souhaitons échanger avec vous sur votre parcours professionnel",
                 cta: {
                   href: "https://www.example.com",
                   type: "local",
@@ -137,9 +157,23 @@ const Home = () => (
                 },
               },
               {
-                heading: "Aliquam condimentum interdum",
+                heading: "Entretien face à face",
                 copy:
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et ultricies est. Mauris iaculis eget dolor nec hendrerit. Phasellus at elit sollicitudin, sodales nulla quis, consequat libero.",
+                  "Rencontre avec votre éventuel futur manager pour détailler plus en détails le poste et établir un véritable échange",
+                cta: {
+                  href: "https://www.example.com",
+                  type: "local",
+                  copy: "Lorem ipsum dolor",
+                },
+                pictogram: {
+                  src: Touch,
+                  "aria-label": "Touch",
+                },
+              },
+              {
+                heading: "Validation de la candidature et intégration",
+                copy:
+                  "Félicitations, votre candidature est retenue !",
                 cta: {
                   href: "https://www.example.com",
                   type: "local",
@@ -161,36 +195,73 @@ const Home = () => (
                 sources: [
                   {
                     src:
-                      "https://dummyimage.com/320x180/ee5396/161616&text=16:9",
+                      "https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
                     breakpoint: 320,
                   },
                   {
                     src:
-                      "https://dummyimage.com/400x225/ee5396/161616&text=16:9",
+                      "https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
                     breakpoint: 400,
                   },
                   {
                     src:
-                      "https://dummyimage.com/672x378/ee5396/161616&text=16:9",
+                      "https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
                     breakpoint: 672,
                   },
                 ],
                 alt: "Image alt text",
                 defaultSrc:
-                  "https://dummyimage.com/672x378/ee5396/161616&text=16:9",
+                  "https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
               },
             },
-            heading: "Content Group - Simple",
+            heading: "Témoignages",
             items: [
               {
-                heading: "Lorem ipsum dolor sit amet.",
+                heading: "M - Manager opérationnel itinérant",
                 copy:
                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed interdum tortor. Sed id pellentesque diam. In ut quam id mauris finibus efficitur quis ut arcu. Praesent purus turpis, venenatis eget odio et, tincidunt bibendum sem. Curabitur pretium elit non blandit lobortis. Donec quis pretium odio, in dignissim sapien.",
               },
               {
-                heading: "Lorem ipsum dolor sit amet.",
+                heading: "M - Manager de la conduite d'activités",
                 copy:
                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed interdum tortor. Sed id pellentesque diam. In ut quam id mauris finibus efficitur quis ut arcu. Praesent purus turpis, venenatis eget odio et, tincidunt bibendum sem. Curabitur pretium elit non blandit lobortis. Donec quis pretium odio, in dignissim sapien.",
+              },
+            ],
+          },
+          {
+            type: "ContentGroupSimple",
+            mediaType: "image",
+            mediaData: {
+              heading: "Lorem ipsum dolor sit amet.",
+              image: {
+                sources: [
+                  {
+                    src:
+                      "https://media.istockphoto.com/photos/young-woman-entering-authorization-code-picture-id822123538?s=612x612",
+                    breakpoint: 320,
+                  },
+                  {
+                    src:
+                      "https://media.istockphoto.com/photos/young-woman-entering-authorization-code-picture-id822123538?s=612x612",
+                    breakpoint: 400,
+                  },
+                  {
+                    src:
+                      "https://media.istockphoto.com/photos/young-woman-entering-authorization-code-picture-id822123538?s=612x612",
+                    breakpoint: 672,
+                  },
+                ],
+                alt: "Image alt text",
+                defaultSrc:
+                  "https://media.istockphoto.com/photos/young-woman-entering-authorization-code-picture-id822123538?s=612x612",
+              },
+            },
+            heading: "Pourquoi nous rejoindre",
+            items: [
+              {
+                //heading: "Pour toutes ces raisons :",
+                copy:
+                  "Nous sommes une entreprise dynamique et en perpétuelle évolution. OIS est à l’écoute de ses salariés. Nous développons les compétences de nos collaborateurs. Nos techniciens sont autonomes. Nous disposons d’un CE. Nous avons mis en place le 1% logement. Participation aux bénéfices",
               },
             ],
           },
@@ -473,7 +544,7 @@ const Home = () => (
             ]}
             cta={{
               type: "feature",
-              heading: "Feature Card",
+              heading: "Témoignages",
               card: {
                 cta: {
                   href: "https://www.example.com",
@@ -481,7 +552,8 @@ const Home = () => (
                     src: ArrowRight20,
                   },
                 },
-                heading: "Consectetur adipisicing elit",
+                heading: "M - Manager opérationnel itinérant",
+                copy: "Lorem ipsum",
                 image: {
                   defaultSrc:
                     "https://dummyimage.com/672x672/ee5396/161616&text=1x1",
