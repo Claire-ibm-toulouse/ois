4e1ed4aa615e8fc54a08571848f79b54d9691db2
Claire Lebastard <clebastard@fr.ibm.com>
2021-03-22 17:16:52 +0100

    feat: text updates

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ const Home = () => (
       type="centered"
       theme="g100"
       title="Offres Innovantes de Services (OIS)"
-      copy="Une société de services du groupe IBM France spécialisée dans l'installation de systèmes d'alarmes chez les particuliers et dans la maintenance informatique des agences bancaires."
+      copy="Une société de services du groupe IBM France spécialisée dans l'installation de systèmes d'alarmes chez les particuliers et dans la maintenance informatique pour agences bancaires et commerçants."
       gradient={true}
       // buttons={buttons}
       image={{
@@ -51,11 +51,11 @@ const Home = () => (
         
         // Module "A propos"
         heading="A propos"
-        copy="OIS (Offres Innovantes de Services) est une jeune entreprise dynamique portée par deux grandes entités internationales. 
-        Depuis ses débuts, l'entreprise a bien grandi et cherche encore à évoluer en même temps que ses salariés. 
-        Et pour cela, elle reste à l'écoute de ses collaborateurs. 
-        OIS est répartie en 3 activités principales : l'installation de systèmes d'alarmes chez les particuliers, 
-        la maintenance informatique dans des agences bancaires et chez des commercants, et une cellule transport."
+        copy="Créée en 2017, OIS (Offres Innovantes de Services) est une jeune entreprise dynamique portée par deux grandes entités internationales. 
+        Depuis ses débuts, l'entreprise s’est dotée de compétences techniques de proximité pour accompagner ses clients dans la transformation digitale de leurs entreprises.  
+        Les équipes de techniciens prennent en charge les missions avec, comme principe directeur, la qualité du service rendu. Pour cela, elle reste à l'écoute de ses collaborateurs. 
+        OIS couvre 3 activités principales : l'installation de systèmes d'alarmes principalement chez les particuliers, l'installation et
+        la maintenance de solutions informatiques et monétiques dans des agences bancaires et chez des commercants, et des activités de transport. Notre réponse au marché s’articule autour de trois thèmes : présence géographique, industrialisation et innovation constante."
         
         items={[
           
@@ -68,7 +68,7 @@ const Home = () => (
                 heading:
                   "Technicien itinérant en systèmes d’alarmes",
                 copy:
-                  "Nos technicien(ne)s itinérants installent des systèmes d’alarmes chez des particuliers et des petites entreprises. Principales missions : rédiger le diagnostic sécuritaire en y consignant les souhaits et réserves du client, réaliser les tests attestant le bon fonctionnement du système d’alarme installé, expliquer le fonctionnement du système à l’abonné, intervenir en cas de défauts techniques du système chez l’abonné, assurer le remplacement des pièces.",
+                  "Nos technicien(ne)s itinérants installent des systèmes d’alarmes chez des particuliers et entreprises. Principales missions : rédiger le diagnostic sécuritaire en y consignant les besoins du client, réaliser les tests attestant le bon fonctionnement du système d’alarme installé, expliquer le fonctionnement du système à l’abonné, intervenir en cas de défauts techniques du système chez l’abonné, assurer le remplacement des pièces.",
                 cta: {
                   href: "#nousrejoindre",
                 },
@@ -77,7 +77,7 @@ const Home = () => (
                 heading:
                   "Technicien itinérant en informatique et monétique",
                 copy: 
-                  "Nos technicien(ne)s interviennent dans des succursales bancaires ou commerçants en garantissant la qualité de service et la satisfaction de nos clients. Principales missions: assurer des missions d’installation et/ou de dépannage selon les instructions et les procédures définies, conseiller et assister les utilisateurs, procéder aux diagnostics des pannes, appliquer une procédure de tests aux différents stades de l’intervention, réaliser les actions administratives, informatiques et logistiques.",
+                  "Nos technicien(ne)s interviennent dans des succursales bancaires ou commerçants en garantissant la qualité de service et la satisfaction de nos clients. Principales missions: assurer des tâches d’installation et dépannage selon les instructions et procédures définies, conseiller et assister les utilisateurs, procéder aux diagnostics des pannes, appliquer une procédure de tests aux différents stades de l’intervention, réaliser les actions administratives, informatiques et logistiques.",
                 cta: {
                   href: "#nousrejoindre",
                 },
@@ -118,7 +118,7 @@ const Home = () => (
             items: [
               {
                 copy:
-                  "Nous sommes une entreprise dynamique, à taille humaine et en perpétuelle évolution. Soucieux de respecter l'équilibre vie privée vie professionnelle, nous travaillons en confiance avec nos collaborateurs et vous accordons une grande autonomie. Le développement de vos compétences par des formations adaptées et continues tout au long de votre parcours professionnel, ainsi que des conditions motivantes : CE, 1% Logement, participation aux bénéfices, vous placent au coeur de la stratégie de l'entreprise. Nous recrutons partout en France alors n'attendez plus : rejoignez-nous!",
+                  "Nous sommes une entreprise dynamique, à taille humaine et en constante évolution. Soucieux de respecter l'équilibre vie privée vie professionnelle, nous travaillons en confiance avec l'ensemble de nos collaborateurs et leur accordons une grande autonomie. Nous assurons le développement de leurs compétences par des formations adaptées et continues tout au long de leur parcours professionnel. Nous offrons des conditions motivantes : participation aux bénéfices, CE, 1% Logement, etc. OIS place ses salariés et leur développement au coeur de sa stratégie. Nous recrutons partout en France alors n'attendez plus : rejoignez-nous!",
               },
               {
                 cta: {
@@ -192,7 +192,7 @@ const Home = () => (
                   {
                     heading: "Entretien face à face",
                     copy:
-                      "Rencontre avec votre éventuel futur manager pour détailler plus en détails le poste et établir un véritable échange.",
+                      "Rencontre avec votre éventuel futur manager pour aborder plus en détails les contours du poste et établir un véritable échange.",
                     pictogram: {
                       src: PartnerRelationship,
                       "aria-label": "PartnerRelationship",
@@ -233,7 +233,7 @@ const Home = () => (
                     heading: 
                       "Abdel - Technicien itinérant en systèmes d’alarmes",
                     copy:
-                      "\"Ce que j'aime dans mon métier est que chaque journée est différente. J'apprécie aussi l’autonomie, la relation client, l’aspect technico-commercial, la proximité avec mes collaborateurs (manager, RH, conduite d'activité ...) toujours à l'écoute pour apporter des solutions.\"",
+                      "\"Ce que j'aime dans mon métier : chaque journée est différente. J'apprécie l’autonomie, la relation client, l’aspect technico-commercial, la proximité avec mes collègues (techniciens, managers, RH, conduite d'activité ...), être toujours à l'écoute pour apporter des solutions.\"",
                     cta: {
                       href: "#nousrejoindre",
                     },
@@ -241,23 +241,23 @@ const Home = () => (
                   {
                     heading: "Olivier - Technicien itinérant en informatique et monétique",
                     copy:
-                      "\"Le meilleur moment de ma journée c'est de terminer mon intervention avec la satisfaction du Client. Ce que j'aime le plus dans mon métier : l'autonomie, la diversité des activités et des échanges, la possibilité de monter en compétences sur différents projets, travailler en itinérance et découvrir notre région au travers de nos tournées.\"",
+                      "\"Le meilleur moment de ma journée : terminer mon intervention avec un client satisfait. Ce que j'aime le plus dans mon métier : l'autonomie, la diversité des activités et des échanges, la possibilité de monter en compétences sur différents projets et lignes de produits, travailler en itinérance et découvrir notre région au travers de nos tournées.\"",
                     cta: {
                       href: "#nousrejoindre",
                     },
                   },
                   {
-                    heading: "Mouna - Manager de la conduite d'activités",
+                    heading: "Mouna - Manageur de la conduite d'activités",
                     copy:
-                      "\"Je suis manager d'une équipe de 4 personnes. Pour réussir dans mon métier, il faut être à l'écoute, avoir un bon esprit d'analyse et une volonté de s'améliorer. J'apprécie de faire monter en compétences mes collaboratrices, évoluer à leur côté et contribuer à la bonne marche de mon entreprise.\"",
+                      "\"Après avoir débuté comme pilote d’activité, j’ai pris le poste de manageur d'une équipe de 4 personnes. Pour réussir dans mon métier, il faut savoir écouter, posséder un bon esprit d'analyse et la volonté constante de progresser. Je saisis chaque occasion pour accompagner mes collaborateurs/trices à monter en compétences, et ainsi contribue à la bonne marche de la société.\"",
                     cta: {
                       href: "#nousrejoindre",
                     },
                   },
                   {
-                    heading: "Ludovic - Manager opérationnel",
+                    heading: "Ludovic - Manageur opérationnel",
                     copy:
-                      "\"Je m'occupe d'une équipe de 20 techniciens (6 en informatique et 14 en alarme). Pour exercer mon métier, il faut être à l'écoute, disponible, communiquer, connaître le métier des techniciens. J'aime apporter des réponses et solutions à mes collaborateurs. J'apprécie la diversité de mes activités et l'appui des deux grandes entités de notre entreprise.\"",
+                      "\"J’ai commencé chez OIS en 2017 comme technicien itinérant basé sur Nantes. Depuis plus de 2 ans, j’ai été promu manageur d'une équipe de 20 techniciens (6 en informatique et 14 en alarme). Pour exercer mon métier, il faut être à l'écoute, se rendre disponible, communiquer, comprendre le métier des techniciens, leur apporter des solutions. J'apprécie la diversité de mes activités et l'appui des deux grandes entités de notre entreprise.\"",
                     cta: {
                       href: "#nousrejoindre",
                     },
